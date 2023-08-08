7ca2b83047b6096cc12b2688ac59331fca752bf6
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2023-08-08 16:41:43 +0530

    Autoformatting + SIGTERM handler

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-require('dotenv').config();
-const fs = require('fs');
+require("dotenv").config();
+const fs = require("fs");
 const http = require("http");
 
-const host = 'localhost';
+const host = "localhost";
 const port = 8000;
 
 const requestListener = function (req, res) {
-	res.setHeader("Content-Type", "application/json");
-	res.writeHead(200);
-        res.end(JSON.stringify({message: fs.readFileSync('./main.txt', {encoding: 'utf-8'})}));
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(
+    JSON.stringify({
+      message: fs.readFileSync("./main.txt", { encoding: "utf-8" }),
+    })
+  );
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-    fs.writeFileSync('./pid', '' + process.pid);
+  console.log(`Server is running on http://${host}:${port}`);
+  fs.writeFileSync("./pid", "" + process.pid);
+});
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    fs.unlinkSync("./pid");
+    process.exit(0);
+  });
 });
